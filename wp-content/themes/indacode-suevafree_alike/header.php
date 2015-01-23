5faba18473fc4121d8afad5fc27315ab8099e75c
Patrick Mazulo <pmazulo@gmail.com>
2015-01-23 18:44:46 -0300

    "Deleted the comment's plugin."

--- wp-content/themes/indacode-suevafree_alike/header.php
+++ wp-content/themes/indacode-suevafree_alike/header.php
@@ -19,24 +19,7 @@
 <![endif]-->
 
 <?php wp_head(); ?>
-<script type="text/javascript">
-//<![CDATA[
-  (function() {
-    var shr = document.createElement('script');
-    shr.setAttribute('data-cfasync', 'false');
-    shr.src = '//dsms0mj1bbhn4.cloudfront.net/assets/pub/shareaholic.js';
-    shr.type = 'text/javascript'; shr.async = 'true';
-    shr.onload = shr.onreadystatechange = function() {
-      var rs = this.readyState;
-      if (rs && rs != 'complete' && rs != 'loaded') return;
-      var site_id = '164454f073ffd30e3c1206b182b1c352';
-      try { Shareaholic.init(site_id); } catch (e) {}
-    };
-    var s = document.getElementsByTagName('script')[0];
-    s.parentNode.insertBefore(shr, s);
-  })();
-//]]>
-</script>
+
 </head>
 <body <?php body_class('custombody'); ?>>
 
